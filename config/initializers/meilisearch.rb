56ef2774e0b613ea1ea6fca3fc1976ef5fda7cb3
miroslav <miroslav@kolosek.com>
2023-07-04 09:32:05 +0200

    change config

--- config/initializers/meilisearch.rb
+++ config/initializers/meilisearch.rb
@@ -1,4 +1,4 @@
 MeiliSearch::Rails.configuration = {
-  meilisearch_url: 'http://localhost:7700',
+  meilisearch_url: ENV.fetch('MEILISEARCH_URL', 'http://localhost:7700'),
   meilisearch_api_key: 'BmfZvLvNKhgRtwkN1PgZ4EValccnjRUxu1xdzvn453M',
 }
